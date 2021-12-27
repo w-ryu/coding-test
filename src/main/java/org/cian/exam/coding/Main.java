9f83a4a12ffa03dcece1f5c9e9736939e42ee326
cianryu <rmsceo1226@gmail.com>
2021-12-27 17:56:41 +0900

    하노이

--- src/main/java/org/cian/exam/coding/Main.java
+++ src/main/java/org/cian/exam/coding/Main.java
@@ -1,10 +1,30 @@
 package org.cian.exam.coding;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static StringBuffer sb = new StringBuffer();
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int total = (int)Math.pow(2, N) - 1;
+        sb.append(total).append("\n");
+        hanoi(N, 1,2,3);
+        System.out.println(sb);
+    }
+
+    static void hanoi(int N, int start, int via, int end) {
+        if(N == 1) {
+            sb.append(start + " " + end).append("\n");
+            return;
+        }
+
+        hanoi(N - 1, start, end, via);
+
+        sb.append(start + " " + end).append("\n");
+        hanoi(N - 1 , via, start, end);
     }
 }

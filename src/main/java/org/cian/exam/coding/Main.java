b947e66535ab3fc805a562e496226219196082b5
cianryu <rmsceo1226@gmail.com>
2022-01-06 16:47:25 +0900

    변경 테스트

--- src/main/java/org/cian/exam/coding/Main.java
+++ src/main/java/org/cian/exam/coding/Main.java
@@ -11,32 +11,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Solution0000 solutionClass = new Solution0000();
         int[] s = {1,2,3,4};
-        int[] answer = solutionClass.solution(s);
+        solutionClass.solution(s);
         //System.out.println(answer);
-        for(int a : answer) {
-            System.out.print(a + ", ");
-        }
+
     }
 }
 
 class Solution0000 {
-    public int[] solution(int[] arr) {
-        int[] answer;
-        if(arr.length == 1) {
-            return answer = new int[]{-1};
-        }
+    public void solution(int[] arr) {
 
-        int min = Arrays.stream(arr).min().getAsInt();
-        int index = 0;
-        answer = new int[arr.length - 1];
-        for(int i = 0 ; i < arr.length ; i++) {
-            if(arr[i] == min){
-                continue;
-            }
-            answer[index] = arr[i];
-            index++;
-        }
-
-        return answer;
     }
 }

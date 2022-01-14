34267891397bf0d829e0686a1e63eb677dd576ea
cianryu <rmsceo1226@gmail.com>
2022-01-14 14:42:12 +0900

    연속 경우의 수까지 완

--- src/main/java/org/cian/exam/coding/programmers/Level2.java
+++ src/main/java/org/cian/exam/coding/programmers/Level2.java
@@ -1,8 +1,61 @@
 package org.cian.exam.coding.programmers;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Level2 {
+
+
+    public int solution12924(int n) {
+        int answer = 0;
+        for(int i = 1 ; i <= n ; i++) {
+            int sum = 0;
+            int num = i;
+            while(sum < n) {
+                sum += num;
+                num++;
+            }
+            if(sum == n){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    //주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수는 주어진 수의 홀수 약수의 개수와 같다라는 정수론 정리가 있음.
+    //주어진 자연수의 홀수 갯수로 확인
+
+    //피보나치 BigInteger
+    public int solution12945(int n) {
+        BigInteger answer = BigInteger.valueOf(0);
+        BigInteger f1 = BigInteger.valueOf(0);
+        BigInteger f2 = BigInteger.valueOf(1);
+        BigInteger num = BigInteger.valueOf(1234567);
+        if(n == 2) return f2.intValue();
+        int count = 1;
+        while(count < n) {
+            answer = f1.add(f2);
+            f1 = f2;
+            f2 = answer;
+            count++;
+        }
+
+        return (answer.remainder(num)).intValue();
+    }
+
+    //행렬의 곱셈
+    public int[][] solution12949(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i = 0 ; i < arr1.length ; i++) {
+            for(int j = 0 ; j < arr2[0].length ; j++) {
+                for(int k = 0 ; k < arr2.length ; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return answer;
+    }
+
     //N개의 최소공배수
     public int solution12953(int[] arr) {
         int answer = 0;
--- src/main/java/org/cian/exam/coding/programmers/Main.java
+++ src/main/java/org/cian/exam/coding/programmers/Main.java
@@ -1,7 +1,10 @@
 package org.cian.exam.coding.programmers;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 public class Main {
@@ -9,31 +12,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Solution0000 solutionClass = new Solution0000();
         Print print = new Print();
-        String s = "3people unFollowed me";
-//        int n = 10;
-        int[] arr1 = {2,6,8,14};
-        int[] arr2 = {27 ,56, 19, 14, 14, 10};
-        int[][] sizes = {{2, 5, 3},{4, 4, 1},{1, 7, 3}};
+        String s = "-1 -2 -3 -4";
+        int n = 15;
+        int[] arr1 = {1,2};
+        int[] arr2 = {3,4};
+        int[][] sizes1 = {{1,4},{3,2},{4,1}};
+        int[][] sizes2 = {{3,3},{3,3}};
         String[] strArr1 = {"marina", "josipa", "nikola", "vinko", "filipa"};
         String[] strArr2 = {"josipa", "filipa", "marina", "nikola"};
 
-        String answer = solutionClass.solution(s);
+        int answer = solutionClass.solution(n);
         print.print(answer);
 
     }
 }
 class Solution0000 {
-    public String solution(String s) {
-        String[] strArr = s.split(" ");
-        StringBuffer sb = new StringBuffer();
-        for(int i = 0 ; i < strArr.length ; i++) {
-            char[] charArr = strArr[i].toLowerCase(Locale.ROOT).toCharArray();
-            if(charArr[0] < '0' || charArr[0] > '9'){
-                charArr[0] = Character.toUpperCase(charArr[0]);
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1 ; i <= n ; i++) {
+            int sum = 0;
+            int num = i;
+            while(sum < n) {
+                sum += num;
+                num++;
             }
-            sb.append(new String(charArr)).append(" ");
+            if(sum == n){
+                answer++;
+            }
         }
-        return sb.toString().trim();
+        return answer;
     }
 }
 
@@ -41,6 +48,10 @@ class Print {
     public void print(int answer) {
         System.out.println(answer);
     }
+
+    public void print(long answer) { System.out.println(answer);}
+
+    public void print(BigInteger answer) { System.out.println(answer);}
 
     public void print(String answer) {
         System.out.println(answer);

816dada2285cc6181b10174a02574e91a27e00ec
cianryu <rmsceo1226@gmail.com>
2022-01-15 16:48:52 +0900

    백준 연산자 끼워넣기 중~

--- src/main/java/org/cian/exam/coding/baekjoon/Main.java
+++ src/main/java/org/cian/exam/coding/baekjoon/Main.java
@@ -6,22 +6,50 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static int N = 9;
-    public static int count = 0;
-    public static int[][] sudokuArr;
-    public static int[] numArr;
+    public static long max = Integer.MIN_VALUE;
+    public static long min = Integer.MAX_VALUE;
+    public static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sudokuArr = new int[N][N];
-
+        N = Integer.parseInt(br.readLine());
+        String[] strArr = br.readLine().split(" ");
+        String[] codeStrArr = br.readLine().split(" ");
+        int[] numArr = new int[N];
+        int[] codeArr = new int[4];
         for(int i = 0 ; i < N ; i++) {
-            String[] inputArr = br.readLine().split(" ");
-            for(int j = 0 ; j < N ; j++) {
-                sudokuArr[i][j] = Integer.parseInt(inputArr[j]);
-                if(Integer.parseInt(inputArr[j]) == 0){
-                    count++;
+            numArr[i] = Integer.parseInt(strArr[i]);
+        }
+        for(int i = 0 ; i < 4 ; i++) {
+            codeArr[i] = Integer.parseInt(codeStrArr[i]);
+        }
+        mathMethod(numArr, codeArr, numArr[0], 1);
+
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+    }
+
+    public static void mathMethod(int[] numArr, int[] codeArr, long sum, int depth) {
+        if(depth == N-1) {
+            for(int i = 0 ; i < 4 ; i++){
+                if(codeArr[i] == 1) {
+                    switch(i) {
+                        case 0 :
+                            sum += numArr[depth];
+                            break;
+                        case 1 :
+                            sum -= numArr[depth];
+                            break;
+                        case 2 :
+                            sum *= numArr[depth];
+                            break;
+                        case 3 :
+                            sum /= numArr[depth];
+                            break;
+                    }
                 }
             }
+            if(max < sum) max = sum;
+            if(min > sum) min = sum;
         }
     }
 }

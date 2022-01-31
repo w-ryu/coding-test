aa002a98919311f7c752785fe4f4b1dd885922d3
cianryu <rmsceo1226@gmail.com>
2022-01-31 16:48:14 +0900

    제목 : 알고리즘 내용 : 이친수[완]

--- src/main/java/org/cian/exam/coding/baekjoon/level/foundation1/dynamic1_400/Baek2193.java
+++ src/main/java/org/cian/exam/coding/baekjoon/level/foundation1/dynamic1_400/Baek2193.java
@@ -1,10 +1,13 @@
-package org.cian.exam.coding.baekjoon;
+package org.cian.exam.coding.baekjoon.level.foundation1.dynamic1_400;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+/**
+ * [이친수] 14188KB / 120ms / 544B
+ */
+public class Baek2193 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
